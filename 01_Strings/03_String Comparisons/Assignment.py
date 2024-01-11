760b719690b5c5d09305daf780c438a1891ec9bb
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-01-11 08:10:50 -0500

    updates

---
+++ 01_Strings/03_String Comparisons/Assignment.py
@@ -0,0 +1,3 @@
+text1 = input("Enter text:")
+text2 = input("Enter some more text:")
+print(text1 == text2)
---
+++ 01_Strings/04_Join, Split, and Sort Strings/Assignment.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+
+sep = " "
+full_name = sep.join([first_name, last_name])
+print(full_name)
