08735bc317dc6b00cb67b9ee80e0a89386091bfa
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-02-07 08:01:00 -0500

    l6

---
+++ 06_Types, Modules, and Packages/02_Modules, Import and Math/assignment.py
@@ -0,0 +1,10 @@
+import math
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter a second number: "))
+
+leg1 = number1
+leg2 = number2
+
+hyp = math.hypot(leg1, leg2)
+
+print("The hypotenuse is", hyp)
--- 06_Types, Modules, and Packages/03_Platform and Random Modules/assignment.py
+++ 06_Types, Modules, and Packages/03_Platform and Random Modules/assignment.py
@@ -1,2 +1,7 @@
-#Create a random number generator that simulates a dice roll
+import random
 
+dice_rolls = [1, 2, 3, 4, 5, 6]
+
+random_roll = random.choice(dice_rolls)
+
+print("You rolled a", random_roll)
