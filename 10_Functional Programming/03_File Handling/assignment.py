af62ad3ab9a870f7b03247fdaecb255b32f56d7d
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-03-22 07:42:36 -0400

    up

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,4 +1,9 @@
-file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\myFile.txt"
+file_path = "C:\\Users\\polusmal001\\DEV\\POC_Sem2_Assignments-2\\10_Functional Programming\\03_File Handling\\myFile.txt"
 
+
+file_path = open('myFile.txt')
+print(file_path.read())
+file_path.close()
 ##YOUDO rest of the program for reading and printing to the console
 ##remember pass in file_path to open 
+printstream.read
