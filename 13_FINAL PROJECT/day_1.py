8cadc537b2718ea7a0cb42573505a0d1e5ed691b
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-05-06 08:14:42 -0400

    up

---
+++ 13_FINAL PROJECT/day_1.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+lives = 3
+
+root = tk.Tk()
+
+frame = tk.Frame(root)
+
+canvas = tk.Canvas(frame, width=600, height=400, bg="#AAAAFF")
+frame.pack()
+canvas.pack()
+root.title("BREAKOUT")
+root.mainloop
